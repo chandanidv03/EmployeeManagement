7fbfaebb47e197d0fb6ddc5f8393ec37d5da08d1
= <darshanvd94@gmail.com>
2021-03-20 17:03:50 +0530

    Inital commit

---
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Controllers
+{ 
+    public class HomeController:Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        public ViewResult Index()
+        {
+            IEnumerable<Employee> model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+        [Route("Home/Details/{id?}")]
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+            };
+            //Employee model = _employeeRepository.GetEmployee(1);
+
+            //ViewBag.Employee = model;
+            //ViewBag.PageTitle = "Employee Details";
+
+            //ViewData["Employee"] = model;
+            //ViewData["PageTitle"] = "Employee Details";
+            return View(homeDetailsViewModel);
+        }
+        [Route("Home/Create")]
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if(ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+
+               // return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+            return View();
+           
+        }
+    }
+}
---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",ErrorMessage ="Invalid Email format")]
+        [Display(Name="Office Email")]
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public Dept? Department { get; set; }
+    }
+}
